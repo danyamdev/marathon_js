021cf19e0b82128d40804cfc64704b3ab8dae1f9
danyamamin99 <danya.mamin.99@bk.ru>
2021-09-24 14:03:10 +0300

    add files slider and change README

---
+++ slider/app.js
@@ -0,0 +1,29 @@
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const downBtn = document.querySelector('.down-button');
+const upBtn = document.querySelector('.up-button');
+const container = document.querySelector('.container');
+
+const slidesCount = mainSlide.querySelectorAll('div').length;
+let activeSlide = 0;
+
+sidebar.style.top =  `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => changeSlide('up'));
+downBtn.addEventListener('click', () => changeSlide('down'));
+
+function changeSlide(direction) {
+
+  if (direction === 'up') {
+    activeSlide++;
+    if (activeSlide === slidesCount) activeSlide = 0;
+  } else if (direction === 'down') {
+    activeSlide--;
+    if (activeSlide < 0) activeSlide = slidesCount - 1;
+  }
+
+  const height = container.clientHeight;
+
+  mainSlide.style.transform = `translateY(-${activeSlide * height}px)`;
+  sidebar.style.transform = `translateY(${activeSlide * height}px)`;
+}
